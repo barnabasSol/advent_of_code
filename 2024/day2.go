225085e5061f86b308fe97ee3a392923a8f2b1be
barnabas <xobarneysolomon@gmail.com>
2025-03-28 09:34:48 +0300

    day2p2 done

--- 2024/day2.go
+++ 2024/day2.go
@@ -2,44 +2,141 @@ package twentyfour
 
 import (
 	"bufio"
+	"log"
 	"os"
 	"slices"
 	"strings"
 )
 
-func damp(input []int, i, j int) []int {
-	out := []int{}
-	copy(out, input)
+func damp(reports []int, i, z int) bool {
+	out := make([]int, len(reports))
+	copy(out, reports)
+	out = slices.Delete(out, i, z)
+	is_increasing, is_decreasing := false, false
+	is_safe := true
+	leng := len(out) - 1
 
+	is_safe = damp_check(leng, out, is_increasing, is_decreasing, is_safe)
+	if is_safe {
+		log.Println("is safe")
+		log.Println(reports)
+		log.Println(i, z)
+		log.Println("//////////////")
+		return true
+	}
+
+	is_increasing, is_decreasing = false, false
+	out = make([]int, len(reports))
+	copy(out, reports)
+	out = slices.Delete(out, z, z+1)
+	is_safe = true
+	is_safe = damp_check(leng, out, is_increasing, is_decreasing, is_safe)
+	log.Println(is_safe)
+	log.Println(reports)
+	log.Println(i, z)
+	log.Println("//////////////")
+	return is_safe
 }
 
-func IsSafeWithDampner(reports [][]int) int {
+func damp_check(
+	leng int,
+	out []int,
+	is_increasing, is_decreasing, is_safe bool,
+) bool {
+	for j := range leng {
+		if j == leng {
+			break
+		}
+		check := out[j] - out[j+1]
+		if check < 0 {
+			is_increasing = true
+		} else if check > 0 {
+			is_decreasing = true
+		} else {
+			is_safe = false
+			break
+		}
+		if is_increasing && is_decreasing {
+			is_safe = false
+			break
+		}
+		if is_increasing {
+			if check*-1 > 3 {
+				is_safe = false
+				break
+			}
+		} else if is_decreasing {
+			if check > 3 {
+				is_safe = false
+				break
+			}
+		}
+	}
+	return is_safe
+}
+
+func SafeCountWithDampner(reports [][]int) int {
 	safe := 0
 	for _, x := range reports {
-		leng := len(x)
-		damp_count := 0
-		slices.Sort(x)
-		for j := range leng - 1 {
+		is_increasing, is_decreasing := false, false
+		leng := len(x) - 1
+		for j := range leng {
 			if j == leng {
 				break
 			}
 			check := x[j] - x[j+1]
-			if check*-1 > 3 || check == 0 {
-				damp_count += 1
-				damp(x, j, j+1)
+			if check < 0 {
+				is_increasing = true
+			} else if check > 0 {
+				is_decreasing = true
+			} else {
+				is_safe := damp(x, j, j+1)
+				if is_safe {
+					break
+				}
+				safe -= 1
+				break
 			}
-
+			if is_increasing && is_decreasing {
+				is_safe := damp(x, j, j+1)
+				if is_safe {
+					break
+				}
+				safe -= 1
+				break
+			}
+			if is_increasing {
+				if check*-1 > 3 {
+					is_safe := damp(x, j, j+1)
+					if is_safe {
+						break
+					}
+					safe -= 1
+					break
+				}
+			} else if is_decreasing {
+				if check > 3 {
+					is_safe := damp(x, j, j+1)
+					if is_safe {
+						break
+					}
+					safe -= 1
+					break
+				}
+			}
 		}
+		safe += 1
 	}
 	return safe
 }
 
-func IsSafe(reports [][]int) int {
+func SafeCount(reports [][]int) int {
 	safe := 0
 	for _, x := range reports {
-		leng := len(x)
 		is_increasing, is_decreasing := false, false
-		for j := range leng - 1 {
+		leng := len(x) - 1
+		for j := range leng {
+
 			if j == leng {
 				break
 			}
--- main.go
+++ main.go
@@ -2,19 +2,23 @@ package main
 
 import (
 	"log"
+	"time"
 
 	twentyfour "github.com/barnabasSol/advent/2024"
 )
 
 func main() {
-	reports := [][]int{
-		{7, 6, 4, 2, 1},
-		{1, 2, 7, 8, 9},
-		{9, 7, 6, 2, 1},
-		{1, 3, 2, 4, 5},
-		{8, 6, 4, 4, 1},
-		{1, 3, 6, 7, 9},
-	}
-	log.Println(twentyfour.IsSafe(reports))
+	start := time.Now()
+	// reports := [][]int{
+	// 	{7, 6, 4, 2, 1},
+	// 	{1, 2, 7, 8, 9},
+	// 	{9, 7, 6, 2, 1},
+	// 	{1, 3, 2, 4, 5},
+	// 	{8, 6, 4, 4, 1},
+	// 	{1, 3, 6, 7, 9},
+	// }
+	reports := twentyfour.Day2Input()
 
+	log.Println(twentyfour.SafeCountWithDampner(reports))
+	log.Println(time.Since(start))
 }
